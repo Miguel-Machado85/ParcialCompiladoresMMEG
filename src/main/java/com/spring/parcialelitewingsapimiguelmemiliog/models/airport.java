1871651db80f4ed951f1e29b1e41fdbc51a152ac
Gasvi281 <egaviria011@gmail.com>
2025-03-06 19:44:25 -0500

    entidad aeropuerto

---
+++ src/main/java/com/spring/parcialelitewingsapimiguelmemiliog/models/airport.java
@@ -0,0 +1,31 @@
+package com.spring.parcialelitewingsapimiguelmemiliog.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= "airport")
+public class airport {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private String id;
+
+    @Column(unique = true, nullable=false)
+    private String nombre;
+
+    @Column(unique = true, nullable=false)
+    private String direccion;
+
+    @Column(unique = true, nullable=false)
+    private int capacidad;
+
+    @Column(unique = true, nullable=false)
+    private String dueños="Sofia and Natalia the best owners";
+
+}
